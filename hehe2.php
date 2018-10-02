6a7a3453926e8880909d73054325c491f169cc16
Kevin JG <dev.kjudhist@gmail.com>
2018-10-02 21:23:31 +0700

    -

--- hehe2.php
+++ hehe2.php
@@ -3,11 +3,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script> -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/sample/anu.css">
     <title>PENGPOOOOOL</title>
 </head>
 <body>
@@ -21,27 +16,28 @@
         if(isset($_POST['tambah'])){
             ?>
             <form action="" method="POST">
-            Luas Bangunan/Orang : <input type="number" name="luasBangunan"><br>
-            Bahan Bakar Memasak : <input list="bahanBakar" name="bahanBakar">
+            Luas Bangunan/Orang : <input type="number" name="luasBangunan" required><br>
+            Bahan Bakar Memasak : <input list="bahanBakar" name="bahanBakar" required>
                     <datalist id="bahanBakar">
                         <option value="Kayu Bakar">
+                        <option value="Kompor Listrik">
                         <option value="Gas LPG">
                     </datalist> <br>
-            Jenis Lantai : <input list="jenisLantai" name="jenisLantai">
+            Jenis Lantai : <input list="jenisLantai" name="jenisLantai" required>
                     <datalist id="jenisLantai">
                         <option value="Ubin">
                         <option value="Plester">
                         <option value="Tanah">
                     </datalist> <br>
-            Frekuensi Memakan Daging/Minggu : <input type='number' name='makanDaging'><br>
-                <input type="submit" name="tambahData" value="TAMBAH PAK EKOOOO!!!">
+            Frekuensi Memakan Daging/Minggu : <input type='number' name='makanDaging' required><br>
+                <input type="submit" name="tambahData" value="TAMBAH DATA BARU">
             </form>
         <?php
         }
         if(isset($_POST['tambahData'])){
+            $kategori = getKategori($_POST['luasBangunan'], $_POST['bahanBakar'], $_POST['jenisLantai'], $_POST['makanDaging']);
             $filename = "data.txt";
             $fh = fopen($filename, 'a') or die("can't open file");
-            $kategori = getKategori($_POST['luasBangunan'], $_POST['bahanBakar'], $_POST['jenisLantai'], $_POST['makanDaging']);
             $stringData = ";".$_POST['luasBangunan'].",".$_POST['bahanBakar'].",".$_POST['jenisLantai'].",".$_POST['makanDaging'].",".$kategori;
             fwrite($fh, $stringData);
             fclose($fh);
@@ -62,28 +58,13 @@
                 $dataTabel[$i] = explode(",", $dataTabel[$i]);
             }
 
-            /* echo "<table border='1'>
+            echo "<table border='1'>
                     <th>No</th>
                     <th>Luas Bangunan/Orang</th> 
                     <th>Bahan Bakar Memasak</th> 
                     <th>Jenis Lantai</th>
                     <th>Frekuensi Memakan Daging/Minggu</th>
-                    <th>Kategori</th>"; */
-            echo '<div class="container">
-                    <div class="row">
-                    <div class="table-responsive">
-                    <table id="main-table" class="table exotic-table">
-                    <thead>
-                    <tr class="text-center">
-                        <th>No</th>
-                        <th>Luas Bangunan/Orang</th>
-                        <th>Bahan Bakar Memasak</th>
-                        <th>Jenis Lantai</th>
-                        <th>Frekuensi Memakan Daging/Minggu</th>
-                        <th>Kategori</th>
-                    </tr>
-                    </thead>
-                    <tbody class="text-center">';
+                    <th>Kategori</th>";
 
             for ($i=0; $i < count($dataTabel); $i++) {
                 $j=0;
@@ -95,22 +76,39 @@
                         "<td>".$dataTabel[$i][$j]."</td>".
                     "</tr>";
             };
-            echo "</tbody>
-                    </table>
-	                </div>
-                    </div>";
+            echo "</table>";
         }
 
         function resetData(){
             $filename = "data.txt";
             $fh = fopen($filename, 'w+') or die("can't open file");
-            $stringData = "9,Kayu bakar,Ubin,3,Sedang;10,Gas LPG,Ubin,2,Sedang;15,Gas LPG,Plester,2,Sedang;30,Gas LPG,Ubin,4,Kaya;16,Kompor Listrik,Ubin,3,Kaya;25,Gas LPG,Ubin,5,Kaya;9,Gas LPG,Plester,0.5,Miskin;8,Kayu Bakar,Tanah,1,Miskin;10,Kayu Bakar,Tanah,2,Miskin;14,Gas LPG,Tanah,1,Miskin";
+            $stringData = "9,Kayu Bakar,Ubin,3,Sedang;10,Gas LPG,Ubin,2,Sedang;15,Gas LPG,Plester,2,Sedang;30,Gas LPG,Ubin,4,Kaya;16,Kompor Listrik,Ubin,3,Kaya;25,Gas LPG,Ubin,5,Kaya;9,Gas LPG,Plester,0.5,Miskin;8,Kayu Bakar,Tanah,1,Miskin;10,Kayu Bakar,Tanah,2,Miskin;14,Gas LPG,Tanah,1,Miskin";
             fwrite($fh, $stringData);
             fclose($fh);
             header("Refresh:0");
         }
 
         function getKategori($luasBangunan, $bahanBakar, $jenisLantai, $makanDaging){
+            $posterionMiskin = hitungPosterion("Miskin", $luasBangunan, $bahanBakar, $jenisLantai, $makanDaging);
+            $posterionSedang = hitungPosterion("Sedang", $luasBangunan, $bahanBakar, $jenisLantai, $makanDaging);
+            $posterionKaya = hitungPosterion("Kaya", $luasBangunan, $bahanBakar, $jenisLantai, $makanDaging);
+
+            if($posterionMiskin > $posterionSedang){
+                if($posterionMiskin > $posterionKaya){
+                    return 'Miskin';
+                } else {
+                    return 'Kaya';
+                }
+            }else {
+                if($posterionSedang > $posterionKaya){
+                    return 'Sedang';
+                }else {
+                    return 'Kaya';
+                }
+            }
+        }
+
+        function hitungLikelihood($item, $colomn, $kategori){
             $filename = "data.txt";
             $data = file_get_contents($filename);
             $dataTabel = explode(";", $data);
@@ -119,29 +117,66 @@
                 $dataTabel[$i] = explode(",", $dataTabel[$i]);
             }
 
-            $likelihoodLuasBangunan=0;
-            $likelihoodBahanBakar=0;
-            $likelihoodJenisLantai=0;
-            $likelihoodMakanDaging=0;
+            $total = 0; //total disini maksudnya banyaknya nilai item yang bernilai sesuai dengan data
+            $totalItem=0;
+            for ($i=0; $i < count($dataTabel); $i++) {
+                if($colomn==0 || $colomn==3){
+                    $totalItem += $dataTabel[$i][$colomn];
+                } 
+                if(($item == $dataTabel[$i][$colomn]) && ($kategori == $dataTabel[$i][4])){
+                    $total+=1;
+                }
+            }
+            
+            
+            //hitung likelihood
+            if($total == 0){
+                if($colomn==0 || $colomn==3){
+                    //hitung varian
+                    //digunakan untuk menghitung likelihood data kontinyu
+                    $rataItem = (Double)$totalItem/count($dataTabel);
+                    $nilaiAtas = 0;
+                    for ($i=0; $i < count($dataTabel); $i++) {
+                        $nilaiAtas += (Double)pow((Double)($dataTabel[$i][$colomn]/$rataItem), 2);
+                    }
+                    $varian = (Double)$nilaiAtas/(count($dataTabel)-1);
 
-            for ($i=0; $i < count($dataTabel[0]); $i++) { 
-                if($luasBangunan === $dataTabel[0][$i]){
-                    $likelihoodLuasBangunan++;
-                }
-                if($bahanBakar === $dataTabel[1][$i]){
-                    $likelihoodBahanBakar++;
-                }
-                if($jenisLantai === $dataTabel[2][$i]){
-                    $likelihoodJenisLantai++;
-                }
-                if($luasBangunan === $dataTabel[3][$i]){
-                    $likelihoodMakanDaging++;
+                    return (Double)pow(exp(1), (-1*pow((Double)($item-$rataItem), 2))) / (Double)sqrt(2*pi()*$varian);
+                }else{
+                    return 0;
+                }                
+            }else{
+                return (Double)$total/count($dataTabel);
+            }
+        }
+        
+        function hitungPrior($kategori){
+            $filename = "data.txt";
+            $data = file_get_contents($filename);
+            $dataTabel = explode(";", $data);
+            
+            for ($i=0; $i < count($dataTabel); $i++) { 
+                $dataTabel[$i] = explode(",", $dataTabel[$i]);
+            }
+
+            $banyakKategori = 0;
+            for ($i=0; $i < count($dataTabel); $i++) { 
+                if($kategori === $dataTabel[$i][4]){
+                    $banyakKategori++;
                 }
             }
 
-            if($likelihoodLuasBangunan === 0){
-                
-            }
+            return (Double)$banyakKategori/count($dataTabel);
+        }
+
+        function hitungPosterion($kategori, $luasBangunan, $bahanBakar, $jenisLantai, $makanDaging){
+            $prior = hitungPrior($kategori);
+            $likelihoodLuasBangunan = hitungLikelihood($luasBangunan, 0, $kategori);
+            $likelihoodBahanBakar = hitungLikelihood($bahanBakar, 1, $kategori);
+            $likelihoodJenisLantai = hitungLikelihood($jenisLantai, 2, $kategori);
+            $likelihoodMakanDaging = hitungLikelihood($makanDaging, 3, $kategori);
+
+            return (Double)$likelihoodLuasBangunan*$likelihoodBahanBakar*$likelihoodJenisLantai*$likelihoodMakanDaging*$prior;
         }
     ?>
 </body>
---
+++ pengpol2/heheMe.php
@@ -0,0 +1,168 @@
+<html lang="en">
+    <head>
+            <link rel="stylesheet" type="text/css" href="split.css">
+    </head>
+    <body>
+        <div class="split1 left">
+            <h1>
+                Blackpink Generator
+            </h1>
+            <h3>
+                seberapa miskin kah anda?
+            </h3>
+            <div class="center">
+                    <?php showData();?>    
+                    <form action="" method="POST">
+                        <br><input type="submit" name="tambah" value="Tambah Data">
+                        <input type="submit" name="resetData" value="Reset Data">
+                    </form>
+                    <?php
+                        if(isset($_POST['tambah'])){
+                            ?>
+                            <div name="inputan">
+                            <form action="" method="POST">
+                            Luas Bangunan/Orang : <input type="number" name="luasBangunan"><br>
+                            Bahan Bakar Memasak : <input list="bahanBakar" name="bahanBakar">
+                                    <datalist id="bahanBakar">
+                                         <option value="Kayu Bakar">
+                                        <option value="Gas LPG">
+                                    </datalist> <br>
+                            Jenis Lantai : <input list="jenisLantai" name="jenisLantai">
+                                    <datalist id="jenisLantai">
+                                        <option value="Ubin">
+                                        <option value="Plester">
+                                        <option value="Tanah">
+                                    </datalist> <br>
+                            Frekuensi Memakan Daging/Minggu : <input type='number' name='makanDaging'><br>
+                                <input type="submit" name="tambahData" value="TAMBAH PAK EKOOOO!!!">
+                            </form>
+                            </div>
+                        <?php
+                        }
+                        if(isset($_POST['tambahData'])){
+                            $filename = "data.txt";
+                            $fh = fopen($filename, 'a') or die("can't open file");
+                            $kategori = getKategori($_POST['luasBangunan'], $_POST['bahanBakar'], $_POST['jenisLantai'], $_POST['makanDaging']);
+                            $stringData = ";".$_POST['luasBangunan'].",".$_POST['bahanBakar'].",".$_POST['jenisLantai'].",".$_POST['makanDaging'].",".$kategori;
+                            fwrite($fh, $stringData);
+                            fclose($fh);
+                            header("Refresh:0");
+                        }
+                
+                        if(isset($_POST['resetData'])){
+                            resetData();
+                        }
+                
+                        function showData(){            
+                            $filename = "data.txt";
+                            $data = file_get_contents($filename);
+                
+                            $dataTabel = explode(";", $data);
+                            
+                            for ($i=0; $i < count($dataTabel); $i++) { 
+                                $dataTabel[$i] = explode(",", $dataTabel[$i]);
+                            }
+                            echo '<table id="main-table" class="table exotic-table">
+                                    <thead>
+                                    <tr class="text-center">
+                                        <th>No</th>
+                                        <th>Luas Bangunan/Orang</th>
+                                        <th>Bahan Bakar Memasak</th>
+                                        <th>Jenis Lantai</th>
+                                        <th>Frekuensi Memakan Daging/Minggu</th>
+                                        <th>Kategori</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="text-center">';
+                
+                            for ($i=0; $i < count($dataTabel); $i++) {
+                                $j=0;
+                                echo "<tr><td>".($i+1)."</td>".
+                                        "<td>".$dataTabel[$i][$j++]."</td>".
+                                        "<td>".$dataTabel[$i][$j++]."</td>".
+                                        "<td>".$dataTabel[$i][$j++]."</td>".
+                                        "<td>".$dataTabel[$i][$j++]."</td>".
+                                        "<td>".$dataTabel[$i][$j]."</td>".
+                                    "</tr>";
+                            };
+                            echo "</tbody>
+                                    </table>";
+                        }
+                
+                        function resetData(){
+                            $filename = "data.txt";
+                            $fh = fopen($filename, 'w+') or die("can't open file");
+                            $stringData = "9,Kayu bakar,Ubin,3,Sedang;10,Gas LPG,Ubin,2,Sedang;15,Gas LPG,Plester,2,Sedang;30,Gas LPG,Ubin,4,Kaya;16,Kompor Listrik,Ubin,3,Kaya;25,Gas LPG,Ubin,5,Kaya;9,Gas LPG,Plester,0.5,Miskin;8,Kayu Bakar,Tanah,1,Miskin;10,Kayu Bakar,Tanah,2,Miskin;14,Gas LPG,Tanah,1,Miskin";
+                            fwrite($fh, $stringData);
+                            fclose($fh);
+                            header("Refresh:0");
+                        }
+                
+                        function getKategori($luasBangunan, $bahanBakar, $jenisLantai, $makanDaging){
+                            $filename = "data.txt";
+                            $data = file_get_contents($filename);
+                            $dataTabel = explode(";", $data);
+                            
+                            for ($i=0; $i < count($dataTabel); $i++) { 
+                                $dataTabel[$i] = explode(",", $dataTabel[$i]);
+                            }
+                
+                            $likelihoodLuasBangunan=0;
+                            $likelihoodBahanBakar=0;
+                            $likelihoodJenisLantai=0;
+                            $likelihoodMakanDaging=0;
+                
+                            for ($i=0; $i < count($dataTabel[0]); $i++) { 
+                                if($luasBangunan === $dataTabel[0][$i]){
+                                    $likelihoodLuasBangunan++;
+                                }
+                                if($bahanBakar === $dataTabel[1][$i]){
+                                    $likelihoodBahanBakar++;
+                                }
+                                if($jenisLantai === $dataTabel[2][$i]){
+                                    $likelihoodJenisLantai++;
+                                }
+                                if($luasBangunan === $dataTabel[3][$i]){
+                                    $likelihoodMakanDaging++;
+                                }
+                            }
+                
+                            if($likelihoodLuasBangunan === 0){
+                                
+                            }
+                        }
+                    ?>
+            </div>
+          </div>
+          <div class="split2 right">
+            <div>
+                <?php
+                $filename2 = "data.txt";
+                $data2 = file_get_contents($filename2);
+    
+                $dataTabel2 = explode(";", $data2);
+                for ($i=0; $i < count($dataTabel2); $i++) { 
+                    $dataTabel2[$i] = explode(",", $dataTabel2[$i]);
+                }
+                if(count($dataTabel2) > 10){
+                $ab = count($dataTabel2)-1;
+
+                echo"
+                <h4>L  = Luas Bangunan / orang</h4>
+                <h4>B  = Bahan bakar memasak</h4>
+                <h4>JL = Jenis Lantai</h4>
+                <h4>M  = Frekuensi makan daging/ minggu</h4>
+                <h4>K  = Kategori</h4>
+                <h2>K = Miskin</h2>
+                <p>P = ( K = Miskin | L = ".$dataTabel2[$ab][0]." ,B = ".$dataTabel2[$ab][1]." ,JL = ".$dataTabel2[$ab][2]." ,M = ".$dataTabel2[$ab][3]." ) = P( L = ".$dataTabel2[$ab][0]." | K=Miskin )*P( B = ".$dataTabel2[$ab][1]." |K= Miskin )*P( JL = ".$dataTabel2[$ab][2]." |K= Miskin )*P(M = ".$dataTabel2[$ab][3]." |K= Miskin )*P(K= Miskin )</p>
+                <h2>K = Sedang</h2>
+                <p>P = ( K = Sedang | L = ".$dataTabel2[$ab][0]." ,B = ".$dataTabel2[$ab][1]." ,JL = ".$dataTabel2[$ab][2]." ,M = ".$dataTabel2[$ab][3]." ) = P( L = ".$dataTabel2[$ab][0]." | K=Sedang )*P( B = ".$dataTabel2[$ab][1]." |K= Sedang )*P( JL = ".$dataTabel2[$ab][2]." |K= Sedang )*P(M = ".$dataTabel2[$ab][3]." |K= Sedang )*P(K= Sedang )</p>
+                <h2>K = Kaya</h2>
+                <p>P = ( K = Kaya | L = ".$dataTabel2[$ab][0]." ,B = ".$dataTabel2[$ab][1]." ,JL = ".$dataTabel2[$ab][2]." ,M = ".$dataTabel2[$ab][3]." ) = P( L = ".$dataTabel2[$ab][0]." | K=Kaya )*P( B = ".$dataTabel2[$ab][1]." |K= Kaya )*P( JL = ".$dataTabel2[$ab][2]." |K= Kaya )*P(M = ".$dataTabel2[$ab][3]." |K= Kaya )*P(K= Kaya )</p>";
+                }
+
+                ?>
+            </div>
+          </div>
+    </body>
+</html>
